8690a94e6624befb688473e454ebd761678f3ad9
Vera E <97448213+ZenonCodes@users.noreply.github.com>
2022-03-25 13:44:06 -0400

    Set up generics

--- src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
+++ src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
@@ -1,7 +1,30 @@
 package com.zipcodewilmington.arrayutility;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by leon on 3/6/18.
  */
-public class ArrayUtility {
+public class ArrayUtility <T> {
+    List<T> x = new ArrayList<T>();
+
+    public ArrayUtility(T[] inputArray) {
+    }
+
+    public T getNumberOfOccurrences(T valueToEvaluate) {
+        return null;
+    }
+
+    public <B extends Object> B countDuplicatesInMerge(T[] arrayToMerge, T valueToEvaluate) {
+        return null;
+    }
+
+    public T[] removeValue(T valueToRemove) {
+        return null;
+    }
+
+    public T getMostCommonFromMerge(T[] arrayToMerge) {
+        return null;
+    }
 }
